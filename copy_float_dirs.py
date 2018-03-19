e93c66e9771a1232b131801a833517498cc5b95b
Patrick Treuthardt <patrick.treuthardt@naturalsciences.org>
2018-03-19 17:10:06 -0400

    modified but not yet functional

--- copy_float_dirs.py
+++ copy_float_dirs.py
@@ -26,7 +26,7 @@ import os.path ## Common pathname manipulations
 import sys ## System-specific parameters and functions
 
 ##
-## Main
+## Global Variables
 ##
 now=datetime.datetime.now() ## gets current date and time
 version='1.0/20180319'
@@ -34,7 +34,8 @@ intern_path='/data1/patrick/PROJECTS/Hurricanes/Pictures/' #internal drive path 
 extern_path='/media/patrick/Hurricane_B/Hurricanes/Pictures/' #external drive path of data
 valid_min_year=1995
 valid_max_year=2005
-print("\nRunning COPY_FLOAT_DIRS.PY version:", version,"\n")
+
+print("\nRunning COPY_FLOAT_DIRS.PY version:", version,"\n") #Let's go!
 
 ##
 ## Check if year choice is valid
@@ -47,12 +48,32 @@ def analyze_year(choice_input):
 ##
 ##
 ##
-def look_for_float_dirs(input):
+def look_for_float_dirs(year_input):
+    path_01=extern_path+str(year_input)+"/" #path to selected year
+    path_02=[] #create empty list
+    path_03=[]
+#    dir_suffix=[]
+    hurr_dirs=os.listdir(path_01) #list directories
+    for i in hurr_dirs: 
+        if fnmatch.fnmatch(i,str(year_input)+"*"): #look for usable dirs
+            path_02.append(path_01+i+"/") #create list of usable dirs
+ #           dir_suffix.append(i+"/")
+ #   print(dir_suffix)
+ #   print()
+    for j in path_02:
+        dirs_in_hurr=os.listdir(j) #list contents of hurr dir
+        for k in dirs_in_hurr: #look at each element in list
+            if fnmatch.fnmatch(k,"FLOAT"): #if FLOAT is in dir
+                path_03.append(j+"FLOAT/") #create list of dirs with FLOAT
+    print(path_03)
+    print()
+    
     return
 
 ##
 ## Main
 ##
-year_choice=input("\nSelect year from 1995 - 2005: ") #ask user which year to process
-
+year_choice=input("\nSelect year from "+str(valid_min_year)+" - "+str(valid_max_year)+": ") #ask user which year to process
+print()
 analyze_year(year_choice) #test year choice
+look_for_float_dirs(year_choice)
